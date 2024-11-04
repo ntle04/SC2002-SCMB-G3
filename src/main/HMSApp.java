22ce5a123554d1673f76d017fe225a3b71bea7e9
ntle04 <natalieng.99@gmail.com>
2024-11-04 11:21:35 +0800

    set up HMSApp package

--- src/main/HMSApp.java
+++ src/main/HMSApp.java
@@ -1,1 +1,19 @@
+package main;
+
+import java.util.Scanner;
+
+enum Role{
+    PHARMACIST,
+    DOCTOR,
+    ADMINISTRATOR,
+    PATIENT
+}
+
+
+public class HMSApp
+{
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+    }
+}
 
