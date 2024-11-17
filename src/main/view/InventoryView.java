f596808c389527eb8943da4aab414e7dd8eb687a
ntle04 <natalieng.99@gmail.com>
2024-11-17 11:10:30 +0800

    fix Inventory MVC

--- src/main/view/InventoryView.java
+++ src/main/view/InventoryView.java
@@ -5,32 +5,21 @@ import main.model.Medicine;
 
 public class InventoryView {
 
-    // // Display the medicine inventory
-    // public void printInventory(Inventory inventory) {    
-    // 	System.out.println("Medication ID: " + inventory.getId());
-    //     System.out.println("Name: " + patient.getContact().getName());
-    //     System.out.println("DOB: " + patient.getContact().getDOB()); 
-    //     System.out.println("Gender: " + patient.getContact().getGender());
-    //     System.out.println("Email: " + patient.getContact().getEmail());
-    //     System.out.println("Blood Type: " + patient.getPatientBloodType());
-    //     System.out.println("Diagnoses: " + patient.getDiagnosis());
-    //     System.out.println("Treatments: " + patient.getTreatment(patient.getId()));
-    // }
+        // Display the medicine inventory
+        public void printInventory(Inventory inventory) {    
+            if(inventory.isEmpty()){
+                System.out.println("Inventory is empty.");
+            }
     
-    // public void printUpdatedPatientContact(Contact contact, String patientId) {
-    // 	System.out.println("Patient ID: " + patientId);
-    //     System.out.println("Email: " + contact.getEmail()); 
-    //     System.out.println("Address: " + contact.getAddress());
-    	
-    // }
- 
-    // public void printUpdateInformation(String email) {
-    // 	System.out.println("Email: " + email); 
-    // }
-    
-    // public String printUpdateConfirmation() {
-    // 	return "Patient information updated successfully!";
-    // }
+            for(Medicine medicine : inventory.getAllMedicines()){
+                System.out.println("Medication ID: " + medicine.getMedId());
+                System.out.println("Name: " + medicine.getMedName());
+                System.out.println("Quantity: " + medicine.getQuantity()); 
+                System.out.println("Sale Price: " + medicine.getSalePrice());
+                System.out.println("Last Purchase: " + medicine.getLastPurchase());
+                System.out.println("Stock Level: " + medicine.getStockLevel());
+            }
+        }
     
     
 }
