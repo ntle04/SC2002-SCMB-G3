593cccbba102ad163fbfb01078549b48750f2dee
Tim <timothyhye01@gmail.com>
2024-11-17 16:28:30 +0800

    add selectedappt implementation

--- src/main/menu/DoctorMenu.java
+++ src/main/menu/DoctorMenu.java
@@ -116,9 +116,9 @@ public class DoctorMenu extends Menu{
                 case 5://Accept or Decline requests
                     apptSlotController.printPendingAppointmentSlots();
                     int selectedSlot = sc.nextInt();
-                    sc.nextLine();
-                    List<AppointmentSlot> slots = apptSlotController.filterSlotsByDoctorId(loggedInUser.getId());
-                    AppointmentSlot chosen = slots.get(selectedSlot - 1);
+                    //List<AppointmentSlot> slots = apptSlotController.filterSlotsByDoctorId(loggedInUser.getId());
+                    //AppointmentSlot chosen = slots.get(selectedSlot - 1);
+                    AppointmentSlot chosen = selectAppointment();
                     System.out.print("Enter A to accept appt and D to decline");
                     String reqchoice = sc.nextLine();
                     char character = reqchoice.charAt(0);
@@ -130,7 +130,7 @@ public class DoctorMenu extends Menu{
                         //availabilitySlotController.cancelAvailabilitySlot(chosen.getAvailabilitySlotId());
                     }
                     else{
-
+                        break;
                     }
                     break;
 
@@ -174,7 +174,7 @@ public class DoctorMenu extends Menu{
         int choice = scanner.nextInt();
 
         // Validate the choice and return the corresponding slot
-        int index = 1;
+        int index = 0;
         for (AppointmentSlot slot : slots) {
             if (index == choice) {
                 System.out.println("Appointment Slot ID: " + slot.getAppointmentSlotId());
