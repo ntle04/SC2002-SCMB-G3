73936e4be73ab29bb64c4554899e38b0a3a383be
Tim <timothyhye01@gmail.com>
2024-11-17 22:03:08 +0800

    done

--- src/main/model/AppointmentSlot.java
+++ src/main/model/AppointmentSlot.java
@@ -70,12 +70,12 @@ import main.util.TimeSlot;
             // this.availabilitySlot = availSlotController.getAvailabilitySlotById(availabilitySlotId);
         }
 
-        public AppointmentSlot(String availabilitySlotId, String patientId) {
+        public AppointmentSlot(String availabilitySlotId, String patientId, ApptStatus status) {
             idCounter = IdGenerator.generateNewId(FILE_PATH);
             this.appointmentSlotId = "AS" + idCounter;
             this.availabilitySlotId = availabilitySlotId;
             this.patientId = patientId;
-            this.status = ApptStatus.PENDING;
+            this.status = status;
             // Load the availability slot when creating appointment slot
             // AvailabilitySlotController availSlotController = new AvailabilitySlotController();
             // this.availabilitySlot = availSlotController.getAvailabilitySlotById(availabilitySlotId);
